a084ea7d90b71472c4fc0ec74384caddc6f90903
nana-718 <75443953+nana-718@users.noreply.github.com>
2021-05-13 04:28:17 +0900

    Update index.js

--- src/pages/Lesson05/index.js
+++ src/pages/Lesson05/index.js
@@ -4,8 +4,25 @@ import Chart from "../../components/Chart05";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  const genders = Array.from(new Set(input.map(({ gender }) => gender)));//性別の配列を作る
+  const min = Math.round(Math.min(...input.map(({ y }) => y)));//身長の最小値を求める
+  const max = Math.round(Math.max(...input.map(({ y }) => y)));//身長の最大値を求める
+  const bins = Array.from({ length: max - min + 1 }).map((_, i) => {//必要な目盛りは最小値の身長から最大値の身長があればよい=>最大値-最小値+1
+    const obj = {
+      bin: (min + i).toString(),
+    };
+    for (const gender of genders) {
+      obj[gender] = 0;
+    }
+    return obj;
+  });
+  for (const { y, gender } of input) {
+    const i = Math.round(y) - min;
+    bins[i][gender] += 1;
+  }
+  return bins;
 };
+
 
 const Lesson = () => {
   return (
