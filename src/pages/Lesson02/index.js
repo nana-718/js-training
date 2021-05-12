a64668715f5363dd2f2b100d3435710ced219d9b
nana-718 <75443953+nana-718@users.noreply.github.com>
2021-05-13 04:25:28 +0900

    Update index.js

--- src/pages/Lesson02/index.js
+++ src/pages/Lesson02/index.js
@@ -4,8 +4,13 @@ import Chart from "../../components/Chart02";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  input.sort((item1, item2) => item2.count - item1.count);
+  return input.slice(0, 20);
 };
+/*
+最初にデータをソートする
+=>上位20件のみなので、ソートした0から20番目の値を切り抜く( input.slice(0, 20);)
+*/
 
 const Lesson = () => {
   return (
