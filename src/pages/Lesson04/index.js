1c63eeffbb95ccad554197b5a168ca44124360b1
nana-718 <75443953+nana-718@users.noreply.github.com>
2021-05-13 04:27:05 +0900

    Update index.js

--- src/pages/Lesson04/index.js
+++ src/pages/Lesson04/index.js
@@ -3,8 +3,16 @@ import LessonPage from "../../components/LessonPage";
 import Chart from "../../components/Chart04";
 import instruction from "./instruction.md";
 
-const convertData = (input) => {
-  return []; // ここを作りましょう！
+const convertData = (input) => {//最初に配列に格納する
+  const species = Array.from(new Set(input.map(({ species }) => species)));//重複なく数えるためにSetを使う
+  return species.map((species) => {//配列speciesに対してmapメッソドを使う
+    return {
+      id: species,
+      data: input
+        .filter((item) => item.species === species)//filter メソッドを使って名前が同じもののみ取り出す
+        .map(({ sepalLength: x, petalWidth: y }) => ({ x, y })), //x、yをそれぞれ変換
+    };
+  });
 };
 
 const Lesson = () => {
